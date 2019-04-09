ec1fe108907f87d2f6b6c92d0754a9950c450495
Natsudark13 <alejandro.r.13@hotmail.com>
2019-04-08 23:19:24 -0600

    a

--- src/conexion/CRUD.java
+++ src/conexion/CRUD.java
@@ -14,7 +14,7 @@ private DBConnection db;
 		this.db = db;
 	}
 	
-	public void insert(String type, String name, String date, String sTime, String fTime, String description) throws SQLException {
+	public void insertActivity(String type, String name, String date, String sTime, String fTime, String description) throws SQLException {
 	 	Connection con = db.openConnection();
         String sql = "insert into " + "Activity" + " values( " + "?,?,?,?,?,?" + ")";
         try (PreparedStatement pstmt = con.prepareStatement(sql)) {
@@ -56,7 +56,7 @@ private DBConnection db;
 	    con.close();
 	}
 	
-	public void delete(String name) throws SQLException {
+	public void deleteActivity(String name) throws SQLException {
 	 	Connection con = db.openConnection();
 	    String sql = "delete from " + "Activity" + " where ActivityName = "+"'"+name+"'"+"";
 	    try (PreparedStatement pstmt = con.prepareStatement(sql)) {
@@ -65,6 +65,98 @@ private DBConnection db;
 	    }
 	    con.close();
 	}
+	
+	public void insertBlock(String topic, String description, String date, String timeZone) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "insert into " + "Block" + " values( " + "?,?,?,?" + ")";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+        	
+        	SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+	  
+	        try {
+	        	java.sql.Date date1 = new java.sql.Date( dateformat.parse(date).getTime());
+
+				
+				pstmt.setObject(1, topic);
+	            pstmt.setObject(2, description);
+	            pstmt.setObject(3, date1);
+	            pstmt.setObject(4, timeZone);
+
+	            pstmt.execute();
+	            
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+	            
+            
+        }
+        con.close();
+    }
+	
+	public void insertBlockActivity(String activityName,String topic) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "insert into " + "Block_Activity" + " values( " + "?,?" + ")";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setObject(1, topic);
+	            pstmt.setObject(2, activityName);
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void insertSupervisor(int id, String sex, String email, String name, String lastN, String lastN2, int exp, String organization) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "insert into " + "Supervisor" + " values( " + "?,?,?,?,?,?,?,?" + ")";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setObject(1, id);
+	            pstmt.setObject(2, sex);
+	            pstmt.setObject(3, email);
+	            pstmt.setObject(4, name);
+	            pstmt.setObject(5, lastN);
+	            pstmt.setObject(6, lastN2);
+	            pstmt.setObject(7, exp);
+	            pstmt.setObject(8, organization);
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void insertUser(int id, String sex, String email, String name, String lastN, String lastN2, String password, String identityP) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "insert into " + "User" + " values( " + "?,?,?,?,?,?,?,?" + ")";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setObject(1, id);
+	            pstmt.setObject(2, sex);
+	            pstmt.setObject(3, email);
+	            pstmt.setObject(4, name);
+	            pstmt.setObject(5, lastN);
+	            pstmt.setObject(6, lastN2);
+	            pstmt.setObject(7, password);
+	            pstmt.setObject(8, identityP);
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void insertComments(String id,String name, String description) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "insert into " + "Comments" + " values( " + "?,?,?" + ")";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setObject(1, id);
+	            pstmt.setObject(2, name);
+	            pstmt.setObject(3, description);
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
 	
 	public void deleteSupervisorActivity(String name,int id) throws SQLException {
 	 	Connection con = db.openConnection();
@@ -75,4 +167,54 @@ private DBConnection db;
 	    }
 	    con.close();
 	}
+	
+	public void deleteBlock(String topic) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "delete from " + "Block" + " where Topic = "+"'"+topic+"'"+"";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void deleteBlockActivity(String topic, String activityName) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "delete from " + "Block_Activity" + " where Topic = "+"'"+topic+"'"+"and ActivityName = "+"'"+activityName+"'"+"";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void deleteSupervisor(int id) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "delete from " + "Supervisor" + " where SupervisorID = "+id+"";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void deleteUser(int id) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "delete from " + "User" + " where UserID = "+id+"";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
+	
+	public void deleteComment(String id,String activityName) throws SQLException {
+	 	Connection con = db.openConnection();
+        String sql = "delete from " + "Comments" + " where UserID = "+id+"and ActivityName = "+"'"+activityName+"'"+"";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	            pstmt.execute();	                                 
+        }
+        con.close();
+    }
 }
--- src/conexion/Main.java
+++ src/conexion/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String [ ] args)
 	{
 		DBConnection db = new DBConnection();
-		ActivityCrud x = new ActivityCrud(db);
+		CRUD x = new CRUD(db);
 		try {
-			x.delete("Activida Informativa");
+			x.deleteActivity("Activida Informativa");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
