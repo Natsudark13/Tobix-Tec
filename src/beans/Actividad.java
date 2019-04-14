6259691bb83bbde743ee58ee6e6706c917069f78
Natsudark13 <alejandro.r.13@hotmail.com>
2019-04-14 17:43:34 -0600

    a

--- src/beans/Actividad.java
+++ src/beans/Actividad.java
@@ -1,19 +1,22 @@
 package beans;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+
+import conexion.CRUD;
 
 public class Actividad {
 	private String nombreActividad;
 	private String tipoActividad;
-	private Date fechaActividad;
+	private String fechaActividad;
 	private String horaInicio;
 	private String horaFinal;
 	private String descripcionActividad;
 	private ArrayList <Comentario> comentarios;
 	private ArrayList <Usuario> encargados;
 	
-	public Actividad(String pNombreActividad, String pTipoActividad, Date pFechaActividad, String pHoraInicio, String pHoraFinal, String pDescripcionActividad){
+	/*public Actividad(String pNombreActividad, String pTipoActividad, Date pFechaActividad, String pHoraInicio, String pHoraFinal, String pDescripcionActividad){
 		setNombreActividad(pNombreActividad);
 		setTipoActividad(pTipoActividad);
 		setFechaActividad(pFechaActividad);
@@ -21,6 +24,15 @@ public class Actividad {
 		setHoraFinal(pHoraFinal);
 		setDescripcionActividad(pDescripcionActividad);
 		comentarios = new ArrayList<Comentario>();
+	}*/
+	
+	public Actividad() {
+		
+	}
+	
+	public void registrarActividad() throws SQLException {
+		CRUD crud = new CRUD();
+		crud.insertActivity(tipoActividad, nombreActividad, fechaActividad, horaInicio, horaFinal, descripcionActividad);
 	}
 	
 	public String registrarComentario(String pDescripcionComentario){
@@ -34,6 +46,22 @@ public class Actividad {
 		return "Encargado registrado";
 	}
 	
+	public ArrayList<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(ArrayList<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public ArrayList<Usuario> getEncargados() {
+		return encargados;
+	}
+
+	public void setEncargados(ArrayList<Usuario> encargados) {
+		this.encargados = encargados;
+	}
+
 	public String getNombreActividad() {
 		return nombreActividad;
 	}
